726240ba88bc40f8e4d12ca9ba01a7b19ac1bc02
Kai Patel <106317141+KaiPatel22@users.noreply.github.com>
2026-03-22 18:44:39 +0000

    altered the extraction to work as expected and ran

--- code/extractActivityLabels.sh
+++ code/extractActivityLabels.sh
@@ -17,7 +17,7 @@ echo "[*] Output: $OUTPUT_FILE"
     grep -E '^[0-9]+\.[0-9]+ to [0-9]+\.[0-9]+ -' "$TEXT_FILE" | while read line; do
         start=$(echo "$line" | awk '{print $1}')
         end=$(echo "$line" | awk '{print $3}')
-        label=$(echo "$line" | sed 's/^[0-9]* to [0-9]* - //' | sed 's/ /_/g' | tr '[:upper:]' '[:lower:]')
+        label=$(echo "$line" | sed 's/^[0-9.]*  *to  *[0-9.]*  *-  *//' | awk '{for(i=1;i<=NF;i++){if(i==1) printf tolower($i); else printf toupper(substr($i,1,1)) tolower(substr($i,2))} print ""}')
         
         echo -e "\t{\"start\": $start, \"end\": $end, \"label\": \"$label\"},"
     done
